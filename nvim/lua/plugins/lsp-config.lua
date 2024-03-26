d3ed4b5bb03d5be24fa060950db88207aa3509f0
integer2bit <8826286@gmail.com>
2024-03-26 23:55:32 +0800

    add footer

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -34,6 +34,10 @@ return {
 			lspconfig.bashls.setup({
 				capabilities = capabilities,
 			})
+      lspconfig.marksman.setup({
+				capabilities = capabilities,
+			})
+
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
