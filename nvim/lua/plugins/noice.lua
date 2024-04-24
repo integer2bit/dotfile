46eac55ff425f002f2f78012ba404eaa8a459a56
integer2bit <8826286@gmail.com>
2024-04-24 13:39:43 +0800

    update plugins

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -5,11 +5,7 @@ return {
 		-- add any options here
 	},
 	dependencies = {
-		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   `nvim-notify` is only needed, if you want to use the notification view.
-		--   If not available, we use `mini` as the fallback
 		"rcarriga/nvim-notify",
 	},
 	config = function()
@@ -23,6 +19,7 @@ return {
 				},
 			},
 			-- you can enable a preset for easier configuration
+			format = {},
 			presets = {
 				bottom_search = true, -- use a classic bottom cmdline for search
 				command_palette = true, -- position the cmdline and popupmenu together
