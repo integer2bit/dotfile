20583a4823481facd992e30a0d37abf2cc7d9842
integer2bit <8826286@gmail.com>
2024-05-31 00:25:40 +0800

    modfiy wezterm tab bar height

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -82,6 +82,20 @@ config.colors = {
 		},
 	},
 }
+config.window_frame = {
+	-- The font used in the tab bar.
+	-- Roboto Bold is the default; this font is bundled
+	-- with wezterm.
+	-- Whatever font is selected here, it will have the
+	-- main font setting appended to it to pick up any
+	-- fallback fonts you may have used there.
+	font = wezterm.font({ family = "Roboto", weight = "Bold" }),
+
+	-- The size of the font in the tab bar.
+	-- Default to 10.0 on Windows but 12.0 on other systems
+	font_size = 10.0,
+}
+
 config.initial_rows = 30
 config.initial_cols = 120
 -- default shell
