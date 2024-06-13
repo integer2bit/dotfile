92b59bdb60347e8e29331f7a60ad4506b8af776b
integer2bit <8826286@gmail.com>
2024-06-13 13:04:25 +0800

    tweak oil.lua config, remove eslint_d in nvim-lint, update plugins

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -5,12 +5,12 @@ return {
 	config = function()
 		local lint = require("lint")
 		lint.linters_by_ft = {
-			javascript = { "eslint_d" },
-			typescript = { "eslint_d" },
-			javascriptreact = { "eslint_d" },
-			typescriptreact = { "eslint_d" },
 			json = { "jsonlint" },
 			-- markdown = { "markdownlint" },
+			-- javascript = { "eslint_d" },
+			-- typescript = { "eslint_d" },
+			-- javascriptreact = { "eslint_d" },
+			-- typescriptreact = { "eslint_d" },
 			-- python = { "pylint" },
 		}
 		-- local markdownlint = require("lint").linters.markdownlint
@@ -19,17 +19,15 @@ return {
 		-- 	"MD013",
 		-- 	"--", -- Required
 		-- }
-
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
 			group = lint_augroup,
 			callback = function()
 				lint.try_lint()
 			end,
 		})
-		vim.keymap.set("n", "<leader>l", function()
-			lint.try_lint()
-		end, { desc = "Trigger linting for current file" })
+		-- vim.keymap.set("n", "<leader>l", function()
+		-- 	lint.try_lint()
+		-- end, { desc = "Trigger linting for current file" })
 	end,
 }
